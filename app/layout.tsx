c4e77b6f8d1d0d06cb2b7c64ec85a882ac3ae32b
jainendra001 <21je0417@iitism.ac.in>
2025-07-12 15:49:11 +0530

    Added the necessary SEO elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body className={`${merriweather.variable} ${openSans.variable}`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  )
+  <html lang="en" className="scroll-smooth">
+    <head>
+      <meta name="robots" content="index, follow" />
+      <meta property="og:title" content="Prof. Anil K Chaubey - IIT (ISM) Dhanbad" />
+      <meta property="og:description" content="Professional portfolio of Prof. Anil Kumar Chaubey, Visiting Professor at IIT (ISM) Dhanbad" />
+      <meta property="og:url" content="https://portfoilio-ak-chaubey.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <link rel="canonical" href="https://portfoilio-ak-chaubey.vercel.app/" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body className={`${merriweather.variable} ${openSans.variable}`}>
+      <Navbar />
+      {children}
+    </body>
+  </html>
+)
 }
